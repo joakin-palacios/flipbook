53d57fc149ca7ab0a03121c51e95843cb64af24a
joakin-palacios <joakinpalacios@gmail.com>
2025-06-15 18:37:26 +0200

    simple implementation that successfully tested the concept

--- implementation/src/main.py
+++ implementation/src/main.py
@@ -1,80 +1,40 @@
 from machine import ADC, Pin, PWM
-import uasyncio
-# import motor_control as mtrc
-# import neopixels_control as neopc
-# import potentiometer_control as potc
 import neopixel
 import time
-import _thread
-import sys
 
 wait=0.0001
 adc_value=0
 
-async def ledctrl():
+def ledctrl():
+    def time_between_events(freq_hz):
+        """
+        Calculate the time to wait between events at a given frequency.
+
+        :param freq_hz: Frequency in Hertz (Hz)
+        :return: Time between events in seconds
+        """
+        if freq_hz <= 0:
+            raise ValueError("Frequency must be greater than zero.")
+        return 1.0 / freq_hz
     global wait
     BLACK = (0, 0, 0)
     WHITE = (255, 255, 255)
     
-    np = neopixel.NeoPixel(Pin(16), 9) # 10 cuz of nr of leds
+    np = neopixel.NeoPixel(Pin(1), 9) # 10 cuz of nr of leds
     while True:
-        np.fill(BLACK)
-        np.write()
-        await uasyncio.sleep(wait)
         np.fill(WHITE)
         np.write()
-        await uasyncio.sleep(wait)
+        time.sleep(0.000001)
+        np.fill(BLACK)
+        np.write()
+        time.sleep(time_between_events(41))
         
-async def readingpots():
-    global wait, adc_value
     
-    pot = ADC(Pin(26))
-    alpha=0.2
-    filtered_value= 1
-    
-    while True:
-        raw=pot.read_u16()
-        filtered_value = alpha * raw + (1 - alpha) * filtered_value
-        
-        adc_value = int(filtered_value)       
-        await uasyncio.sleep_ms(500)
-    
-async def pwmctrl():
-    global adc_value
-    
-    pwm = PWM(Pin(22))
+def pwmctrl(value):    
+    pwm = PWM(Pin(0))
     pwm.freq(10000)  
+    pwm.duty_u16(value)
     
-    while True:
-         pwm.duty_u16(adc_value)
-         await uasyncio.sleep_ms(500)
-
-# def wait_for_input():
-#     
-#     global wait
-#     try:
-#         wait = int(input("Please set a strobe wait time"))
-#     except:
-#         pass
-
-async def main():
-    global wait, adc_value
-
-    led_control = uasyncio.create_task(ledctrl())
-    pot_reading = uasyncio.create_task(readingpots())
-    pwm_control = uasyncio.create_task(pwmctrl())
-
-
-    while True:
-        
-        await uasyncio.sleep(1)
-        print ("wait=",wait)
-        print ("pmw_pot=",adc_value)
-
-# _thread.start_new_thread(wait_for_input, ())
-try:
-    uasyncio.run(main())
-finally:
-    uasyncio.new_event_loop()
-    
-
+ 
+pwmctrl(59626)
+ledctrl()
